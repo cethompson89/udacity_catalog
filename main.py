87cac256e19444dffcd4dc2b81d47e8f7208fc56
Chris Thompson <ce.thompson89@gmail.com>
2017-06-28 11:59:23 -0400

    Add Google Login Functionality

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import httplib2
 import json
 from flask import make_response
 import requests
-#  from oath_logins import google
+import oath_logins.google as google
 
 app = Flask(__name__)
 
@@ -38,6 +38,32 @@ def showLogin():
     # return "The current session state is %s" % login_session['state']
     return render_template('login.html', state=state)
 
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    returned_state = request.args.get('state')
+    code = request.data
+
+    (failure_reponse, access_token, gplus_id) = google.validate_user(returned_state, code)
+    if failure_reponse:
+        return failure_reponse
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = access_token
+    login_session['gplus_id'] = gplus_id
+
+    (username, picture, email) = google.get_user_info(access_token)
+    login_session['username'] = username
+    login_session['picture'] = picture
+    login_session['email'] = email
+    login_session['provider'] = 'google'
+
+    flash("you are now logged in as %s" % login_session['username'])
+
+    output = "<h1>Welcome, %s!</h1>" % login_session['username']
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
 
 
 
